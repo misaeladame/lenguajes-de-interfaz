9693a243d1989de480aaec0d2be177f479f27476
José Misael Adame Sandoval <misael_adame@protonmail.com>
2021-01-14 23:07:38 -0600

    Add files via upload

---
+++ prog60.asm
@@ -0,0 +1,68 @@
+;Alumno: 18131209 - ADAME SANDOVAL JOSE MISAEL
+
+;prog60.asm
+;Tarea 4.08
+;Programa en lenguaje ensamblador mediante el cual se dibujen en la consola del 
+;DOS 80 barras de colores (8 colores básicos y 8 colores con brillo).
+
+Pinta MACRO Color, eIni, eFin
+	mov ah, 06h
+	mov al, 00h
+	mov bh, Color 
+	mov cx, eIni
+	mov dx, eFin
+	int 10h
+ENDM
+
+Datos MACRO
+	mov ax, @Data 
+	mov ds, ax
+ENDM
+
+ReadKey MACRO
+	mov ah, 08h				
+	int 21h
+ENDM
+
+Mensaje MACRO cadena
+	mov ah, 09h
+	mov dx, offset cadena
+	int 21h
+ENDM
+
+Control MACRO
+	mov ax, 4c00h 
+	int 21h
+ENDM
+
+.MODEL SMALL
+.STACK 20h
+.DATA
+	vContador 	  DB 00h
+	cNombreAlumno DB 0Ah, 0Dh, 'Autor: 18131209 - ADAME SANDOVAL JOSE MISAEL$'
+	cFinProg  	  DB 0Ah, 0Ah, 0Dh, '--------- FIN DEL PROGRAMA ---------$'
+.CODE
+	inicio: 
+		Datos
+		Pinta 07h, 0000h, 184Fh ;limpia la pantalla
+
+		eDibujar:
+			add bh, 10h
+			add cl, 01h
+			add dl, 01h
+			
+			Pinta bh, cx, dx
+			
+			inc vContador
+			
+			cmp vContador, 4Fh
+			jb eDibujar
+			
+		eFinProg:
+			ReadKey
+			Mensaje cNombreAlumno
+			Mensaje cFinProg
+			ReadKey
+			Control
+			
+	END inicio
